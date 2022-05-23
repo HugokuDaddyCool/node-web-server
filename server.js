085a511e88189dd89d802dc9ff27e4a10fe7afca
Victor Cortes <victor.cortes@softtek.com>
2022-05-23 16:35:30 -0500

    Starting Chapter 9

--- server.js
+++ server.js
@@ -1,24 +1,45 @@
-const { application } = require('express');
+const fs = require('fs');
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        console.log('Unable to append to server.log');
+    })
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs')
+});
 app.use(express.static(__dirname + '/public'));
 
-// app.get('/', (req, res) => {
-//     res.send('<h1>Hello Express!</h1>');
-// });
 app.get('/', (req, res) => {
-    res.send({
-        name: 'Victor',
-        likes: [
-            'Biking',
-            'Cities'
-        ]
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website'
     });
 });
 
 app.get('/about', (req, res) => {
-    res.send('About page');
+    // res.send('About page');
+    res.render('about.hbs', {
+        pageTitle: 'About page',
+    })
 });
 
 app.get('/bad', (req, res) => {
